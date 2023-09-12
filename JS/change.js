117af16e7a993c0f5b4867f3f807e712bf45cc6e
JuanP4bl0 <juanp4blog@gmail.com>
2023-09-11 18:46:10 -0600

    Modify some things

--- JS/change.js
+++ JS/change.js
@@ -1,3 +1,5 @@
+//----------------------Manejo de los botones LEND y Borrow-----------------------//
+
 // Función para manejar el clic en "Lend"
 function handleLendClick() {
     // Cambiar el fondo y color del botón "Lend"
@@ -31,4 +33,38 @@ function handleLendClick() {
   // Asignar las funciones a los eventos de clic
   document.querySelector(".options_lend").addEventListener("click", handleLendClick);
   document.querySelector(".options_borrow").addEventListener("click", handleBorrowClick);
-  
+
+
+//---------------------------------------------------------------------------------------------------//
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtén todos los botones "Deny"
+  const denyButtons = document.querySelectorAll("[id^='Deny']");
+
+  // Agrega un evento de clic a cada botón "Deny"
+  denyButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      // Obtén el ID del contenedor padre
+      const containerId = this.getAttribute("data-container-id");
+      
+      // Encuentra el contenedor actual
+      const container = document.querySelector(`.${containerId}`);
+
+      // Verifica si se encontró un contenedor
+      if (container) {
+        // Elimina el contenedor actual
+        container.remove();
+        
+        // Encuentra el siguiente contenedor y hazlo visible con una transición
+        const nextContainerId = parseInt(containerId.split('-')[1]) + 1;
+        const nextContainer = document.querySelector(`.contenedor-${nextContainerId}`);
+        
+        if (nextContainer) {
+          nextContainer.style.transition = "height 0.5s ease-in-out, opacity 0.5s ease-in-out";
+          nextContainer.style.height = "auto";
+          nextContainer.style.opacity = "1";
+        }
+      }
+    });
+  });
+});
